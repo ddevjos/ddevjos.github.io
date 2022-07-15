b85de081e2aabd11df1b51467b69d84bd3fb6834
dvjos <gaitancaneljosealexander@gmail.com>
2022-07-14 22:07:34 -0600

    update

---
+++ scroll/scroll-to-top.js
@@ -0,0 +1,22 @@
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
+
+function goToTop() {
+    document.body.scrollIntoView({
+        behavior: "smooth",
+    });
+}
